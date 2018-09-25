cab79864dc167d0df3657fa06ba2f78b46c810a2
Jbrow95 <browningjustin95@gmail.com>
2018-09-25 12:57:35 -0400

    created board class

---
+++ board.rb
@@ -0,0 +1,9 @@
+class Board
+	
+	def initialize
+		@board = @board = [1,2,3,4,5,6,7,8,9]
+		@wins_combo = [[0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]]
+		@win = false
+
+	end
+end
