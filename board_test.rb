e148a99115042ff9eb59bcd1544b57d5ac56a7eb
Jbrow95 <browningjustin95@gmail.com>
2018-10-06 00:30:23 -0400

    tested if player class does whats suppose to

--- board_test.rb
+++ board_test.rb
@@ -1,4 +1,5 @@
 require_relative 'board.rb'
+require_relative 'player.rb'
 require 'test/unit'
 
 class TestUntitled < Test::Unit::TestCase
@@ -18,5 +19,10 @@ class TestUntitled < Test::Unit::TestCase
     def test_if_board_resets
         c = Board.new
         assert_equal c.reset_board, [1,2,3,4,5,6,7,8,9]
+    end
+
+    def test_if_get_name_gets_name_from_class_player_and_character
+        n = Player.new("John","X")
+        assert_equal n.name, "John"
     end
 end
