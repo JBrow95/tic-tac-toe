7d5cc36d8c8ac248d55cf488c7be7f3b687c89bd
Jbrow95 <browningjustin95@gmail.com>
2018-10-08 13:42:52 -0400

    created seq cpu function to update board based on the computers move

--- computer.rb
+++ computer.rb
@@ -73,5 +73,60 @@ class PlayerComp
         end
     end
 
-    
+    def seq_cpu
+        count = 0
+        marker = nil
+        win_flag = false
+        @c.draw_board
+        while count < 9
+
+                if count % 2 ==0
+                    marker = @player1
+                    char = "X"
+                else
+                    marker = @player2
+                    char ="O"
+                end
+                if marker == @player2
+            var1, var2 = s_move(marker)
+                else
+            var1, var2 = play_move(marker)
+                end
+            update = @c.update_board(var1.to_i,var2)
+            if update == false #stops extra counts 
+                count == count
+            else
+            checking_win = @c.check_win(char)
+            if checking_win == true
+                win(marker)
+            else
+                count += 1
+                if count == 9
+                    puts "Game is a draw"
+                    win("draw")
+                end
+            end
+            end
+
+        end
+    end
+
+    def win(player)
+		if player =="draw"
+			puts "Game was a draw."
+		else
+		  puts "#{player.name} WINS! \nWhat a Battle!"
+		end
+		puts "Press Y to play again, or any other key to quit."
+		answer = gets.chomp.capitalize
+			if answer == "Y"
+				puts "\n\nThe Board has been reset!"
+				@c = Board.new
+                @c.reset_board
+                @c.draw_board
+			else
+				abort ("Thanks for Playing!")
+			end
+        end
+    attr_reader :c
 end
