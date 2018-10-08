f548a91e4f746f8db06503b9a7565eb1ee60bdaa
Jbrow95 <browningjustin95@gmail.com>
2018-10-08 18:23:32 -0400

    created post where i set params to equal choice and used my functions from my classes to choice an option based on computer ai

--- app.rb
+++ app.rb
@@ -5,12 +5,36 @@ enable :sessions
 
 get '/' do
 		
-	session[:board] = Board.new
+	session[:board] = session[:board] || Board.new
+	session[:pvc] = session[:pvc] || PlayerComp.new
 	erb :dashboard, locals:{board: session[:board]}
 end
 
 post '/game' do
+	pvc = session[:pvc]
+	board = session[:board]
+	choice = params[:choice]
+	p choice
 
+	if board.available_options(choice.to_i)
+		board.update_board(choice.to_i, "X")
+		pvc.c_arr << choice
+		board.check_win("X")
+		if board.win
+			redirect '/?win'
+		end
+		var1, var2 = pvc.h_move(Player.new("tom", "O"))
+		board.update_board(var1.to_i, var2)
+		board.check_win("O")
+		if board.win
+			redirect '/?compwin'
+		end
+	else
+		redirect '/?a=b'
+	end
 
+	if board.moves_taken >= 9
+		redirect '/?draw'
+	end
 	redirect '/'
 end
