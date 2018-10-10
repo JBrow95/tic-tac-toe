b4e9ffc5686ca1255cce4c998f52a051a1df1519
Jbrow95 <browningjustin95@gmail.com>
2018-10-09 22:47:31 -0400

    still trying to fix bug

--- app.rb
+++ app.rb
@@ -35,7 +35,7 @@ post '/game' do
 		pvc.c_arr << choice
 		print pvc.c_arr
 		print board.board
-		board.check_win("X")
+		pvc.c.check_win("X")
 		if board.win
 			redirect '/game?playerwins'
 			
@@ -45,7 +45,7 @@ post '/game' do
 		var1, var2 = pvc.h_move(Player.new("Tom", "O"))
 		pvc.c.update_board(var1.to_i, var2)
 		board.update_board(var1.to_i, var2)
-		board.check_win("O")
+		pvc.c.check_win("O")
 		if board.win
 			redirect '/game?ComputerWins'
 		end
@@ -59,11 +59,10 @@ post '/game' do
 end
 
 post '/replay' do
-	board = session[:board]
+	b = session[:board]
 	pvc = session[:pvc]
 	pvc.c.reset_board
-	board.reset_board
-	
+	b.board.reset_board
 	pvc.c_arr.clear
 	redirect '/game'
 end
