ada249cf1064dfe364f068755b43e5355da0a214
Jbrow95 <browningjustin95@gmail.com>
2018-10-10 12:40:44 -0400

    test if hard cpu blocks player across reverse

--- tictactoe_test.rb
+++ tictactoe_test.rb
@@ -258,4 +258,19 @@ class TestUntitled < Test::Unit::TestCase
         pvc.c.update_board(var1.to_i, var2)
         assert_equal var1, 7
     end
+
+    def test_if_hard_cpu_blocks_player_across_reverse
+        pvc = PlayerComp.new
+        choice = 5
+        pvc.c.update_board(choice, "X")
+        pvc.c_arr << choice.to_s
+        var1, var2 = pvc.h_move(Player.new("Tom", "O"))
+        pvc.c.update_board(var1.to_i, var2)
+        choice2 = 7
+        pvc.c.update_board(choice2, "X")
+        pvc.c_arr << choice2.to_s
+        var1, var2 = pvc.h_move(Player.new("Tom", "O"))
+        pvc.c.update_board(var1.to_i, var2)
+        assert_equal var1, 3
+    end
 end
