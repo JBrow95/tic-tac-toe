37affe94a427795afd05c8a259ea97a6e5ea2be3
Jbrow95 <browningjustin95@gmail.com>
2018-10-09 22:42:08 -0400

    trying to fix winning bug

--- board.rb
+++ board.rb
@@ -1,7 +1,7 @@
 class Board
 	attr_accessor :moves_taken, :win, :board
 	def initialize
-		@board = [1,2,3,4,5,6,7,8,9]
+		@board = @board = [1,2,3,4,5,6,7,8,9]
 		@wins_combo = [[0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]]
 		@win = false
 		@moves_taken = 0
@@ -17,7 +17,7 @@ class Board
 	end
 
 	def reset_board
-		@board = [1,2,3,4,5,6,7,8,9]
+		@board = @board = [1,2,3,4,5,6,7,8,9]
 	end
 
 	def update_board(choice,char)
