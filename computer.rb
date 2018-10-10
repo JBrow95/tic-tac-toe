f85cd264c0b1e52836203247b6a87882e262a54e
Jbrow95 <browningjustin95@gmail.com>
2018-10-10 18:58:24 -0400

    fixed my combos in arr

--- computer.rb
+++ computer.rb
@@ -411,28 +411,29 @@ class PlayerComp
         def h_move(player)
             puts "\n#{player.name} Your Turn: #{player.char}." 
             
-            stupid2 = [[6,9,3]]
+            stupid2 = [[9,3,6]]
             stupid = [[2,5,8]]
-            wins_game = [[6,9,3],[1,2,3],[7,4,1],[7,1,4],[9,7,8],[7,9,8],[1,3,2],[5,3,7],[3,6,9],[3,9,6],[9,6,3],[5,4,6],[5,6,4],[5,8,2],[9,3,6],[8,5,2],[6,5,4],[4,5,6],[7,8,9],[9,8,7],[8,9,7],[1,4,7],[5,2,8],[2,5,8],[5,9,1],[1,5,9],[3,5,7],[1,7,4],[7,5,3],[2,9,6],[8,1,4],[3,8,6],[4,6,2],[6,8,3],[3,7,6],[7,3,4],[1,8,4]]
-            middle_corner = [[1, 5, 7], [3, 5, 9], [7, 5, 9],[5,9,3], [9, 5, 3],[1,9,4],[6,1,3],[6,8,9]]
-
+            middle_corner = [[1, 5], [2, 5], [7, 5], [9, 5]]
+            #wins_game = [[6,9,3],[1,2,3],[7,4,1],[7,1,4],[9,7,8],[7,9,8],[1,3,2],[5,3,7],[3,6,9],[3,9,6],[9,6,3],[5,4,6],[5,6,4],[5,8,2],[9,3,6],[8,5,2],[6,5,4],[4,5,6],[7,8,9],[9,8,7],[8,9,7],[1,4,7],[5,2,8],[2,5,8],[5,9,1],[1,5,9],[3,5,7],[1,7,4],[7,5,3],[2,9,6],[8,1,4],[3,8,6],[4,6,2],[6,8,3],[3,7,6],[7,3,4],[1,8,4]]
+            #middle_corner = [[1, 5, 7], [3, 5, 9], [7, 5, 9],[5,9,3], [9, 5, 3],[1,9,4],[6,1,3],[6,8,9]]
+            term = [[1, 2, 3], [2, 3, 1], [4, 5, 6], [5, 6, 4], [7, 8, 9], [8, 9, 7], [1, 4, 7], [4, 7, 1], [2, 5, 8], [5, 8, 2], [3, 6, 9], [6, 9, 3], [3, 5, 7], [5, 7, 3], [1, 5, 9], [5, 9, 1], [1, 3, 2], [1, 7, 4], [1, 9, 5], [2, 8, 5], [3, 9, 6], [3, 7, 5], [4, 6, 5], [7, 9, 8], [4, 2, 1], [2, 6, 3], [8, 6, 9], [4, 8, 7], [1, 8, 4], [1, 6, 2], [4, 3, 2], [3, 8, 6], [2, 7, 4], [6, 7, 8], [4, 9, 8], [2, 8, 6]]
             if @c_arr.include?("5") == false && @c.available_options(5) == true
                 choice = 5
             end
 
-            stupid2.each do |ti|
-                if @c_arr.include?(ti[0].to_s) && @c_arr.include?(ti[1].to_s) && @c.available_options(ti[2]) == true
-                    choice = 3
-                    break
-                end
-            end
+            # stupid2.each do |ti|
+            #     if @c_arr.include?(ti[0].to_s) && @c_arr.include?(ti[1].to_s) && @c.available_options(ti[2]) == true
+            #         choice = 3
+            #         break
+            #     end
+            # end
 
-            stupid.each do |ti|
-                if @c_arr.include?(ti[0].to_s) && @c_arr.include?(ti[1].to_s) && @c.available_options(ti[2]) == true
-                    choice = 8
-                    break
-                end
-            end
+            # stupid.each do |ti|
+            #     if @c_arr.include?(ti[0].to_s) && @c_arr.include?(ti[1].to_s) && @c.available_options(ti[2]) == true
+            #         choice = 8
+            #         break
+            #     end
+            # end
             
             middle_corner.each do |m|
                 if @c_arr.include?(m[0].to_s) && @c_arr.include?(m[1].to_s) && @c.available_options(m[2]) == true
@@ -441,7 +442,7 @@ class PlayerComp
                 end
             end
             
-             wins_game.each do |n|
+             term.each do |n|
                  if @c_arr.include?(n[0].to_s) == true && @c_arr.include?(n[1].to_s) == true  && @c.available_options(n[2]) == true
                      choice = n[2]
                      break
@@ -449,9 +450,9 @@ class PlayerComp
             end
 
             
-            wins_game.each do |c|
+            term.each do |c|
                 if @c_arr.include?(c[0].to_s) == false && @c_arr.include?(c[1].to_s) == false  && @c.available_options(c[0]) == false && @c.available_options(c[1]) == false && @c.available_options(c[2]) == true
-                    choice = c[2]
+                    choice = c[2] 
                     break
                 end
             end
